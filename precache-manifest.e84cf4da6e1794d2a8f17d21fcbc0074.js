30c46af19ec8cbc04a0a46cba7591218f6ff9ee2
KomilT <kaa.96t@gmail.com>
2021-06-06 12:02:24 +0500

    Updates

---
+++ precache-manifest.e84cf4da6e1794d2a8f17d21fcbc0074.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2fc2a8fe08050deda1936c44733556",
+    "url": "/meranda/index.html"
+  },
+  {
+    "revision": "a038a8836fdb4dc760fe",
+    "url": "/meranda/static/css/2.bfbceff5.chunk.css"
+  },
+  {
+    "revision": "da89a159fb24b371c535",
+    "url": "/meranda/static/css/main.00d82384.chunk.css"
+  },
+  {
+    "revision": "a038a8836fdb4dc760fe",
+    "url": "/meranda/static/js/2.037217a9.chunk.js"
+  },
+  {
+    "revision": "a979189b752ee6d79ab214bdfa0c14ea",
+    "url": "/meranda/static/js/2.037217a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da89a159fb24b371c535",
+    "url": "/meranda/static/js/main.608d7c1e.chunk.js"
+  },
+  {
+    "revision": "b3d0ed01b0a2443064c5",
+    "url": "/meranda/static/js/runtime-main.ea23ecd8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/meranda/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/meranda/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/meranda/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/meranda/static/media/slick.f97e3bbf.svg"
+  }
+]);
